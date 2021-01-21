a6640c9643048f1634f90cabc8df363888966bb5
joannamfolk <jfolk2@mail.greenriver.edu>
2021-01-21 14:23:28 -0500

    created index

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+// Turn on error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+/*
+// Define a default route (home page)
+$f3 -> route('GET /', function() {
+    //echo "<h1>Hello, world</h1>";
+    $view = new Template();
+    echo $view -> render('views/home.html');
+});*/
+
+//  Run fat free - has to be the last thing in the file
+$f3->run();
